de18374b144d0401612e958b1c8fca194e9b2bc5
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-11-26 11:09:35 +0100

    performance measurements for different types

--- matrix_matrix_multiplication/Stride.cpp
+++ matrix_matrix_multiplication/Stride.cpp
@@ -1,29 +1,28 @@
 #include <Kokkos_Core.hpp>
 #include <chrono>
 #include <iostream>
-#include <vector>
 
 /**
- * Perform a matrix matrix multiplication.
+ * Check performance of strided memory access.
  */
+template <typename TYPE>
 void stridedAccess()
 {
-    // dimensions
     constexpr size_t M = 4096 * 4096;
     constexpr int iterations = 1;
 
-    // vector y in R^M
-    auto x = Kokkos::View<double *>("A", M * 8);
+    auto x = Kokkos::View<TYPE *>("x", M * 8);
 
     for (auto i = 0; i < iterations; ++i)
     {
         auto start = std::chrono::system_clock::now();
-        for (auto strideExp = -1; strideExp < 4; ++strideExp)
+        for (auto strideExp = 0; strideExp < 4; ++strideExp)
         {
-            auto stride = strideExp < 0 ? 0 : (1 << strideExp);
+            auto stride = (1 << strideExp);
             auto policy = Kokkos::RangePolicy<>(0, M);
             auto kernel = KOKKOS_LAMBDA(const int64_t &idx) { x(idx * stride) += 1; };
-            Kokkos::parallel_for("stride_" + std::to_string(stride), policy, kernel);
+            Kokkos::parallel_for(
+                "stride_" + typeid(TYPE).name() + std::to_string(stride), policy, kernel);
             Kokkos::fence();
         }
         auto stop = std::chrono::system_clock::now();
@@ -37,7 +36,10 @@ int main(int argc, char **argv)
 {
     Kokkos::initialize(argc, argv);
     std::cout << "execution space: " << typeid(Kokkos::DefaultExecutionSpace).name() << std::endl;
-    stridedAccess();
+    stridedAccess<float>();
+    stridedAccess<double>();
+    stridedAccess<int32_t>();
+    stridedAccess<int64_t>();
     Kokkos::finalize();
     return EXIT_SUCCESS;
 }
